8ef25b0d02e872a82fc29f17ace4aa056266f055
Hecktagon <tayler.c.hilton@gmail.com>
2025-11-03 13:20:32 -0700

    Made main function in main.py with main program loop.

--- main.py
+++ main.py
@@ -16,39 +16,53 @@ def is_image_file(file_path):
 def run_script(input_path, function):
     # Convert input to a Path object for easier handling
     path = Path(input_path)
+    outputs = []
 
     # Check if the path is a file and an image
     if path.is_file() and is_image_file(path):
-        function(str(path))  # Process the single file
+        output = function(str(path))  # Process the single file\
+        if output:
+            print(output)
+            outputs.append(output)
     elif path.is_dir():
         # Loop through each file in the directory
         for file in path.iterdir():
             if file.is_file() and is_image_file(file):  # Check if it's a file and an image
-                function(str(file))  # Process each image
+                output = function(str(file))  # Process each image
+                if output:
+                    print(output)
+                    outputs.append(output)
     else:
         print(f'Error: {input_path} is neither a image nor a directory.')
 
-def get_image_path():
+    return outputs
+
+
+def get_path():
     return input('Please enter the image/directory path: ')
 
-if __name__ == '__main__':
+
+def main():
+    results = None;
+
     while True:
-        func = input('Functions:\n-s = Standardize\n-c = Count Red Circles\n-p = Percent Red in Image\n-q = Quit\nPlease enter the function: ')
+        func = input(
+            'Functions:\n-s = Standardize\n-p = Percent Red in Image\n-q = Quit\nPlease enter desired function: ')
 
         if func == '-s':
-            img = get_image_path()
-            run_script(img, standardize_image)
-
-        elif func == '-c':
-            img = get_image_path()
-            run_script(img, red_circles)
+            img = get_path()
+            results = run_script(img, standardize_image)
 
         elif func == '-p':
-            img = get_image_path()
-            run_script(img, percentage_red_pixels)
+            img = get_path()
+            results = run_script(img, percentage_red_pixels)
 
         elif func == '-q':
             break
 
         else:
             print(f'Error: {func} is not a valid function.')
+
+
+if __name__ == '__main__':
+    main()
